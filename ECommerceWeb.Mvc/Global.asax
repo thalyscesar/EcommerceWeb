b64a5515b74a7bd65e243dcdf41e5d343bd104ca
thalyscesar <thalyscesar_@hotmail.com>
2015-04-12 17:47:03 -0300

    Inicial

---
+++ ECommerceWeb.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECommerceWeb.Mvc.MvcApplication" Language="C#" %>
---
+++ ECommerceWeb.Teste/EcommerceTeste.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ECommerceWeb.Teste
+{
+    [TestClass]
+    public class EcommerceTeste
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
